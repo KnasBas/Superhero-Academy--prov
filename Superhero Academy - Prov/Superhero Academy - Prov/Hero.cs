62e95e4cd1a97aa8cb1f9a12a71cd8bcfb1afb67
KnasBas <anton.jansson@elev.ga.ntig.se>
2020-01-27 10:03:06 +0100

    commit 1

---
+++ Superhero Academy - Prov/Superhero Academy - Prov/Hero.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superhero_Academy___Prov
+{
+    class Hero
+    {
+        public string name; //public för att Main ska kunna förändra variabeln
+
+        protected int strength = 0;
+
+        protected int intelligence = 0;
+
+        protected int endurance = 0;
+
+        public Hero() //en konstruktor
+        {
+        }
+    }
+}
---
+++ Superhero Academy - Prov/Superhero Academy - Prov/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Superhero_Academy___Prov
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
