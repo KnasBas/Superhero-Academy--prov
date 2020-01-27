12e6994e788ba1320f8141f04018d82f83e7a83a
KnasBas <anton.jansson@elev.ga.ntig.se>
2020-01-27 10:59:00 +0100

    commit 5

--- Superhero Academy - Prov/Superhero Academy - Prov/Hero.cs
+++ Superhero Academy - Prov/Superhero Academy - Prov/Hero.cs
@@ -10,27 +10,25 @@ namespace Superhero_Academy___Prov
     {
         public string name; //public för att Main ska kunna förändra variabeln
 
-        protected int strength = 0;
-
+        protected int strength = 0;     //
+                                        //
         protected int intelligence = 0; //variablerna är protected för att kunna kommas åt inom subklasserna
+                                        //
+        protected int endurance = 0;    //
 
-        protected int endurance = 0;
+        protected static Random generator = new Random(); //slump generator
 
-        protected static Random generator = new Random(); //slum generator
-
-        protected string classroom;
-
-        private int i = 0;
+        private string classroom; //En private string eftersom ingen i main eller subklasserna behöver åtkomst för parametern, hittils
 
         public Hero()
         {
-            List<string> classrooms = new List<string>() { "Te17A", "Te17B", "Te17C", "Te17D", "Te17E" };
-            classroom = classrooms[generator.Next(classrooms.Count + 1)]; //plus 1 eftersom att jag vill att alla alternativ ska kunna slumpas
-
+            List<string> classrooms = new List<string>() { "Te17A", "Te17B", "Te17C", "Te17D", "Te17E" }; //klassen kommer att slumpas två gånger som koden är uppbyggd nu (när subklassernas instans skapas), men detta märker inte spelaren av.
+            classroom = classrooms[generator.Next(classrooms.Count)]; //jag vill att alla alternativ ska kunna slumpas därför använder jag .Count            
         }
-        public void PrintStats()
+        public void PrintStats() //En enkel metod för att skriva ut den randomiserade stats:en spelarens Hero har fått.
         {
             Console.WriteLine("Namn: " + name);
+            Console.WriteLine("Klass: " + classroom);
             Console.WriteLine("Strength: " + strength);
             Console.WriteLine("Intelligence: " + intelligence);
             Console.WriteLine("Endurance: " + endurance);
--- Superhero Academy - Prov/Superhero Academy - Prov/Program.cs
+++ Superhero Academy - Prov/Superhero Academy - Prov/Program.cs
@@ -10,14 +10,14 @@ namespace Superhero_Academy___Prov
     {
         static void Main(string[] args)
         {
-            Hero h1 = new Hero(); //en ny instans
+            Hero h1 = new Hero(); //en ny instans            
             Console.Write("Döp din hero: ");
             h1.name = Console.ReadLine();
-            Console.WriteLine("Välj vilken klass hen bör vara, 1 = Mutant, 2 = Alien, 3 = Regular");
+            Console.WriteLine("Välj vilken klass hen bör vara, 1 = Mutant, 2 = Alien, 3 = Regular"); //vilka val som finns
             string answer = Console.ReadLine();
-            int choice = 0;
-            bool heroclass = int.TryParse(answer, out choice);
-            while (!heroclass && choice <= 0 || choice >= 4) //felsökning
+            int choice = 0; //parameterns huvudfunktion är att jämföra spelarens alternativ med de giltiga
+            bool heroclass = int.TryParse(answer, out choice); //TryParse för att checka den spelarens alternativ
+            while (!heroclass && choice <= 0 || choice >= 4) //felsökning, while-loopen fortsätter tills du anger ett giltigt svar
             {
                 Console.WriteLine("Ogiltigt svar!");
                 answer = Console.ReadLine();
@@ -27,21 +27,21 @@ namespace Superhero_Academy___Prov
             switch(choice) //En simpel switch för att skriva ut alla olika scenarion för spelaren beroende på vilken typ av hero hen väljer att skapa.
             {
                 case 1:
-                    Mutant m1 = new Mutant();
-                    m1.name = h1.name;
+                    Mutant m1 = new Mutant(); //ny instans av den valda kategorin
+                    m1.name = h1.name; //h1.name ger sin information över till den nya instansen
                     Console.WriteLine("Mutant");
                     m1.PrintStats();
                     break;
 
                 case 2:
-                    Alien a1 = new Alien();
+                    Alien a1 = new Alien(); //ny instans av den valda kategorin
                     a1.name = h1.name;
                     Console.WriteLine("Alien");
                     a1.PrintStats();
                     break;
 
                 case 3:
-                    Regular r1 = new Regular();
+                    Regular r1 = new Regular(); //ny instans av den valda kategorin
                     r1.name = h1.name;
                     Console.WriteLine("Regular");
                     r1.PrintStats();
@@ -49,7 +49,7 @@ namespace Superhero_Academy___Prov
 
             }
 
-            Console.ReadLine();
+            Console.ReadLine(); //Readline i slutet för att läsa av koden ovan.
         }
     }
 }
